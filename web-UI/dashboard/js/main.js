96e7cedc4d92f55d96a92ac1a8d0df9fa4d268b1
Silvio Bacci <ciabbi94@live.it>
2018-02-17 16:32:36 +0100

    Aggiornamento interfaccia

--- web-UI/dashboard/js/main.js
+++ web-UI/dashboard/js/main.js
@@ -15,18 +15,69 @@ var tile_width = 16;
 var tile_height = 16;
 var row_number;
 var col_number;
+var context;
 
 create_texture();
 
-var dam_open = false;
+var dam_open = [false, false, false, false];
 
-var animation_position = 0;
-var animation_timer;
+var animation_position = [0, 0, 0, 0];
+var animation_timer = [0, 0, 0, 0];
+var animation_speed = 50;
 
-var top_dam;
-var bottom_dam;
-var start_dam;
-var end_dam;
+var sd = [0, 24, 0, 16];
+var ed = [6, 29, 12, 29];
+var td = [3, 3, 22, 25];
+var bd = [4, 4, 23, 26];
+
+var d0 = {x: (tile_width - 1) * ed[0], y: tile_height * td[0], w: tile_width * 2, h: tile_height * 3};
+var d1 = {x: tile_width * sd[1], y: tile_height * td[1], w: tile_width * 2, h: tile_height * 3};
+var d2 = {x: (tile_width - 1) * ed[2], y: tile_height * td[2], w: tile_width * 2, h: tile_height * 3};
+var d3 = {x: tile_width * sd[3], y: tile_height * td[3], w: tile_width * 2, h: tile_height * 3};
+
+var g, l, w, r, u, b, w1, w2, w3, w4, g1, g2, g3, g4, u2, b2, o1, o2, o3, o4, o5, o6, o7, o8, o9, o0;
+var t1, t2, t3, m1, m2, m3, m4, m5, m6, m7, m8, f2, f5;
+
+$(document).ready(function(){
+	g = $('#g')[0];
+	l = $('#l')[0];
+	w = $('#w')[0];
+	r = $('#r')[0];
+	u = $('#u')[0];
+	b = $('#b')[0];
+	w1 = $('#1')[0];
+	w2 = $('#2')[0];
+	w3 = $('#3')[0];
+	w4 = $('#4')[0];
+	g1 = $('#5')[0];
+	g2 = $('#6')[0];
+	g3 = $('#7')[0];
+	g4 = $('#8')[0];
+	u2 = $('#u2')[0];
+	b2 = $('#b2')[0];
+	o1 = $('#o1')[0];
+	o2 = $('#o2')[0];
+	o3 = $('#o3')[0];
+	o4 = $('#o4')[0];
+	o5 = $('#o5')[0];
+	o6 = $('#o6')[0];
+	o7 = $('#o7')[0];
+	o8 = $('#o8')[0];
+	o9 = $('#o9')[0];
+	o0 = $('#o0')[0];
+	t1 = $('#t1')[0];
+	t2 = $('#t2')[0];
+	t3 = $('#t3')[0];
+	m1 = $('#m1')[0];
+	m2 = $('#m2')[0];
+	m3 = $('#m3')[0];
+	m4 = $('#m4')[0];
+	m5 = $('#m5')[0];
+	m6 = $('#m6')[0];
+	m7 = $('#m7')[0];
+	f2 = $('#f2')[0];
+	f5 = $('#f5')[0];
+});
 
 // ----------------------------
 // GET USER FUNCTIONS
@@ -105,201 +156,199 @@ function create_texture() {
 	});
 }
 
-function open_dam_left() {
-	var current_index = end_dam - animation_position;
+function open_dam_left(index_dam) {
+	var start_dam = sd[index_dam];
+	var end_dam = ed[index_dam];
+	var top_dam = td[index_dam];
+	var bottom_dam = bd[index_dam];
+	var current_index = end_dam - animation_position[index_dam];
+	
 	switch(current_index) {
 		case end_dam:
-			texture[top_dam][current_index] = "7";
-			texture[bottom_dam][current_index] = "6";
-			animation_position++;
-			draw_texture();
+			context.drawImage(o4, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o5, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
-		case start_dam:
-			if (current_index - 1 > 0) {
-				texture[top_dam][current_index] = "8";
-				texture[bottom_dam][current_index] = "5";
-			}
-			else {
-				texture[top_dam][current_index] = "u";
-				texture[bottom_dam][current_index] = "b";
-			}
-			texture[top_dam][current_index + 1] = "u";
-			texture[bottom_dam][current_index + 1] = "b";
-			animation_position = 0;
-			draw_texture();
-			clearInterval(animation_timer);
+		case end_dam - 1:
+			context.drawImage(g3, (current_index + 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(g2, (current_index + 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o1, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o3, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
+			break;
+		case start_dam - 1:
+			context.drawImage(u, (current_index + 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b, (current_index + 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			dam_open[index_dam] = !dam_open[index_dam];
+			animation_position[index_dam] = 0;
+			clearInterval(animation_timer[index_dam]);
 			break;
 		default:
-			if(current_index + 1 != end_dam) {
-				texture[top_dam][current_index + 1] = "u";
-				texture[bottom_dam][current_index + 1] = "b";
-			}
-			texture[top_dam][current_index] = "a";
-			texture[bottom_dam][current_index] = "d";
-			animation_position++;
-			draw_texture();
+			context.drawImage(u, (current_index + 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b, (current_index + 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o1, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o3, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 	}
-	
-	dam_open = true;
 }
 
-function close_dam_left() {
-	var current_index = start_dam + animation_position;
+function close_dam_left(index_dam) {
+	var start_dam = sd[index_dam];
+	var end_dam = ed[index_dam];
+	var top_dam = td[index_dam];
+	var bottom_dam = bd[index_dam];
+	var current_index = start_dam + animation_position[index_dam];
+	
 	switch(current_index) {
 		case start_dam:
-			texture[top_dam][current_index] = "r";
-			texture[bottom_dam][current_index] = "r";
-			animation_position++;
-			draw_texture();
+			context.drawImage(o1, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o3, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 		case end_dam:
-			texture[top_dam][current_index] = "l";
-			texture[bottom_dam][current_index] = "l";
-			texture[top_dam][current_index - 1] = "9";
-			texture[bottom_dam][current_index - 1] = "0";
-			animation_position = 0;
-			draw_texture();
-			clearInterval(animation_timer);
+			context.drawImage(u2, (current_index - 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b2, (current_index - 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(l, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(l, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			dam_open[index_dam] = !dam_open[index_dam];
+			animation_position[index_dam] = 0;
+			clearInterval(animation_timer[index_dam]);
 			break;
 		default:
-			if (current_index - 1 != start_dam) {
-				texture[top_dam][current_index - 1] = "9";
-				texture[bottom_dam][current_index - 1] = "0";
-			}
-			texture[top_dam][current_index] = "a";
-			texture[bottom_dam][current_index] = "d";
-			animation_position++;
-			draw_texture();
+			context.drawImage(u2, (current_index - 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b2, (current_index - 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o1, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o3, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 	}
-	
-	dam_open = false;
 }
 
-function open_dam_right() {
-	var current_index = start_dam + animation_position;
+function open_dam_right(index_dam) {
+	var start_dam = sd[index_dam];
+	var end_dam = ed[index_dam];
+	var top_dam = td[index_dam];
+	var bottom_dam = bd[index_dam];
+	var current_index = start_dam + animation_position[index_dam];
+	
 	switch(current_index) {
 		case start_dam:
-			texture[top_dam][current_index] = "h";
-			texture[bottom_dam][current_index] = "j";
-			animation_position++;
-			draw_texture();
+			context.drawImage(o6, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o8, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 		case start_dam + 1:
-			texture[top_dam][current_index - 1] = "8";
-			texture[bottom_dam][current_index - 1] = "5";
-			texture[top_dam][current_index] = "k";
-			texture[bottom_dam][current_index] = "m";
-			animation_position++;
-			draw_texture();
+			context.drawImage(g4, (current_index - 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(g1, (current_index - 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o9, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o0, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
-		case end_dam:
-			if(current_index + 1 <= col_number) {
-				texture[top_dam][current_index + 1] = "7";
-				texture[bottom_dam][current_index + 1] = "6";
-			}
-			texture[top_dam][current_index] = "u";
-			texture[bottom_dam][current_index] = "b";
-			texture[top_dam][current_index - 1] = "u";
-			texture[bottom_dam][current_index - 1] = "b";
-			animation_position = 0;
-			draw_texture();
-			clearInterval(animation_timer);
+		case end_dam + 1:
+			context.drawImage(u, (current_index - 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b, (current_index - 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			dam_open[index_dam] = !dam_open[index_dam];
+			animation_position[index_dam] = 0;
+			clearInterval(animation_timer[index_dam]);
 			break;
 		default:
-			texture[top_dam][current_index - 1] = "u";
-			texture[bottom_dam][current_index - 1] = "b";
-			texture[top_dam][current_index] = "k";
-			texture[bottom_dam][current_index] = "m";
-			animation_position++;
-			draw_texture();
+			context.drawImage(u, (current_index - 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b, (current_index - 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o9, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o0, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 	}
-	
-	dam_open = true;
 }
 
-function close_dam_right() {
-	var current_index = end_dam - animation_position;
+function close_dam_right(index_dam) {
+	var start_dam = sd[index_dam];
+	var end_dam = ed[index_dam];
+	var top_dam = td[index_dam];
+	var bottom_dam = bd[index_dam];
+	var current_index = end_dam - animation_position[index_dam];
+	
 	switch(current_index) {
 		case end_dam:
-			if(current_index + 1 <= col_number) {
-				texture[top_dam][current_index + 1] = "l";
-				texture[bottom_dam][current_index + 1] = "l";
-			}
-			texture[top_dam][current_index] = "k";
-			texture[bottom_dam][current_index] = "m";
-			animation_position++;
-			draw_texture();
+			context.drawImage(o9, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o9, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 		case start_dam:
-			texture[top_dam][current_index] = "r";
-			texture[bottom_dam][current_index] = "r";
-			texture[top_dam][current_index + 1] = "9";
-			texture[bottom_dam][current_index + 1] = "0";
-			animation_position = 0;
-			draw_texture();
-			clearInterval(animation_timer);
+			context.drawImage(u2, (current_index + 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b2, (current_index + 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(r, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(r, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			dam_open[index_dam] = !dam_open[index_dam];
+			animation_position[index_dam] = 0;
+			clearInterval(animation_timer[index_dam]);
 			break;
 		default:
-			texture[top_dam][current_index] = "k";
-			texture[bottom_dam][current_index] = "m";
-			texture[top_dam][current_index + 1] = "9";
-			texture[bottom_dam][current_index + 1] = "0";
-			animation_position++;
-			draw_texture();
+			context.drawImage(u2, (current_index + 1) * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(b2, (current_index + 1) * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o9, current_index * tile_width, top_dam * tile_height, tile_width, tile_height);
+			context.drawImage(o0, current_index * tile_width, bottom_dam * tile_height, tile_width, tile_height);
+			animation_position[index_dam]++;
 			break;
 	}
-
-	dam_open = false;
 }
 
-function dam() {
-	start_dam = 0;
-	end_dam = 7;
-	top_dam = 2;
-	bottom_dam = 3;
-	
-	if (dam_open == true)
-		animation_timer = setInterval(close_dam_left, 25);
-	else
-		animation_timer = setInterval(open_dam_left, 25);
+function dam(index_dam, right) {
+	if(right == true) {
+		if (dam_open[index_dam] == true)
+			animation_timer[index_dam] = setInterval(close_dam_right, animation_speed, index_dam);
+		else
+			animation_timer[index_dam] = setInterval(open_dam_right, animation_speed, index_dam);
+	}
+	else {
+		if (dam_open[index_dam] == true)
+			animation_timer[index_dam] = setInterval(close_dam_left, animation_speed, index_dam);
+		else
+			animation_timer[index_dam] = setInterval(open_dam_left, animation_speed, index_dam);
+	}
+}
+
+function contains(rect, x, y) {
+	return (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h)
 }
 
 function draw_texture() {
-	var g = $('#g')[0];
-	var l = $('#l')[0];
-	var w = $('#w')[0];
-	var r = $('#r')[0];
-	var u = $('#u')[0];
-	var b = $('#b')[0];
-	var w1 = $('#1')[0];
-	var w2 = $('#2')[0];
-	var w3 = $('#3')[0];
-	var w4 = $('#4')[0];
-	var g1 = $('#5')[0];
-	var g2 = $('#6')[0];
-	var g3 = $('#7')[0];
-	var g4 = $('#8')[0];
-	var u2 = $('#u2')[0];
-	var b2 = $('#b2')[0];
-	var o1 = $('#o1')[0];
-	var o2 = $('#o2')[0];
-	var o3 = $('#o3')[0];
-	var o4 = $('#o4')[0];
-	var o5 = $('#o5')[0];
-	var o6 = $('#o6')[0];
-	var o7 = $('#o7')[0];
-	var o8 = $('#o8')[0];
-	var o9 = $('#o9')[0];
-	var o0 = $('#o0')[0];
-
 	var canvas = $('#river-ov')[0];
 	canvas.width = tile_width * col_number;
 	canvas.height = tile_height * row_number;
+	
+	context = canvas.getContext("2d");
+	
+	$('#river-ov').mousemove(function(ev) {
+		var x = ev.pageX - $(this).offset().left;
+		var y = ev.pageY - $(this).offset().top;
 
-	var context = canvas.getContext("2d");
+		if(contains(d0, x, y))
+		$(this).css('cursor', 'pointer');
+		else if (contains(d1, x, y))
+		$(this).css('cursor', 'pointer');
+		else if (contains(d2, x, y))
+		$(this).css('cursor', 'pointer');
+		else if (contains(d3, x, y))
+		$(this).css('cursor', 'pointer');
+		else
+		$(this).css('cursor', 'default');
+	});
+	
+	$('#river-ov').click(function(ev) {
+		var x = ev.pageX - $(this).offset().left;
+		var y = ev.pageY - $(this).offset().top;
+
+		if(contains(d0, x, y))
+		dam(0, false);
+		else if (contains(d1, x, y))
+		dam(1, true);
+		else if (contains(d2, x, y))
+		dam(2, false);
+		else if (contains(d3, x, y))
+		dam(3, true);
+	});
 	
 	for(var i = 0; i < row_number; i++) {
 		for(var j = 0; j < col_number; j++) {
@@ -378,6 +427,42 @@ function draw_texture() {
 					break;
 				case "m":
 					context.drawImage(o0, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "n":
+					context.drawImage(t1, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "o":
+					context.drawImage(t2, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "p":
+					context.drawImage(t3, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "q":
+					context.drawImage(m1, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "s":
+					context.drawImage(m2, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "t":
+					context.drawImage(m3, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "v":
+					context.drawImage(m4, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "x":
+					context.drawImage(m5, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "y":
+					context.drawImage(m6, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "z":
+					context.drawImage(m7, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "$":
+					context.drawImage(f2, j*tile_width, i*tile_height, tile_width, tile_height);
+					break;
+				case "!":
+					context.drawImage(f5, j*tile_width, i*tile_height, tile_width, tile_height);
 					break;
 				default:
 					context.drawImage(g, j*tile_width, i*tile_height, tile_width, tile_height);
